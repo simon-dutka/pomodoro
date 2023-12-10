3619f83877a3c40bade00f54590e48c7718b578a
Simon Dutka <Szymon.dutka@outlook.com>
2023-12-10 11:47:10 +0100

    Added Counter

--- src/components/clock/Clock.tsx
+++ src/components/clock/Clock.tsx
@@ -1,12 +1,40 @@
 import React from 'react';
-// import '../../styles/components/clock/clock.scss';
+import '../../styles/components/clock/clock.scss';
+import { useState, useEffect } from 'react';
+
+function MyComponent() {}
+
+const Counter = () => {
+    const [minutesState, setMinutesState] = useState(20);
+    const [secondsState, setSecondsState] = useState(0);
+    let seconds = secondsState;
+    let minutes = minutesState;
+
+    useEffect(() => {
+        const Interval = setInterval(() => {
+            if (seconds == 0) {
+                minutes = minutes - 1;
+                seconds = 59;
+            } else {
+                seconds = seconds - 1;
+            }
+
+            setMinutesState(minutes);
+            setSecondsState(seconds);
+        }, 1000);
+
+        return () => clearInterval(Interval);
+    }, []);
+
+    return (
+        <section className='counter'>
+            {minutesState} {secondsState}
+        </section>
+    );
+};
 
 const Clock = () => {
-    return (
-        <div className=''>
-            <h1>Hello World</h1>
-        </div>
-    );
+    return <div className='clock'>{Counter()}</div>;
 };
 
 export default Clock;
